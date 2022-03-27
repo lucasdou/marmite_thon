55e4b59aee31f792d02f8a3bb5d10b8ef07f0ab1
samuel <samuel.defosse44@gmail.com>
2022-03-27 18:38:35 +0200

    ajout recettes vegan

--- src/mainpackage/RecetteVeganSingleton.java
+++ src/mainpackage/RecetteVeganSingleton.java
@@ -17,6 +17,8 @@ public class RecetteVeganSingleton {
 	private List<Ingredient> ingredientsBurger;
 	private List<Ingredient> ingredientsLasagne;
 	private List<Ingredient> ingredientsGateauYaourtSojaPomme;
+	private List<Ingredient> ingredientsPizzaVeganLegumesDHiver;
+	private List<Ingredient> ingredientsGaspacho;
 	
 	private RecetteVeganSingleton() {
 		
@@ -24,6 +26,9 @@ public class RecetteVeganSingleton {
 		this.ingredientsBurger = new ArrayList<>();
 		this.ingredientsLasagne = new ArrayList<>();
 		this.ingredientsGateauYaourtSojaPomme = new ArrayList<>();
+		this.ingredientsPizzaVeganLegumesDHiver = new ArrayList<>();
+		this.ingredientsGaspacho = new ArrayList<>();
+		
 		this.listRecette = new ArrayList<>();
 		
 		ingredientsSalade.add(new Ingredient("Falafel",333));
@@ -44,10 +49,24 @@ public class RecetteVeganSingleton {
 		ingredientsGateauYaourtSojaPomme.add(new Ingredient("Yaourt soja",46));
 		ingredientsGateauYaourtSojaPomme.add(new Ingredient("Sucre roux",380));
 		
+		ingredientsPizzaVeganLegumesDHiver.add(new Ingredient("Pate à pizza vegan",150));
+		ingredientsPizzaVeganLegumesDHiver.add(new Ingredient("Champignons",22));
+		ingredientsPizzaVeganLegumesDHiver.add(new Ingredient("Epinards frais",23));
+		ingredientsPizzaVeganLegumesDHiver.add(new Ingredient("Persil",43));
+		
+		ingredientsGaspacho.add(new Ingredient("Tomates moyennes", 20));
+		ingredientsGaspacho.add(new Ingredient("Poivrons rouges", 23));
+		ingredientsGaspacho.add(new Ingredient("Concombre", 15));
+		ingredientsGaspacho.add(new Ingredient("Oignon rouge", 36));
+		ingredientsGaspacho.add(new Ingredient("Basilic", 23));
+		ingredientsGaspacho.add(new Ingredient("Persil", 22));
+		
 		this.listRecette.add(new Recette("Salade de falafel", ingredientsSalade, "Entrée", Arrays.asList("Salade","Plat froid")));
 		this.listRecette.add(new Recette("Burger steak carottes", ingredientsBurger, "Plat", Arrays.asList("Burger","Plat chaud")));
 		this.listRecette.add(new Recette("Lasagnes de courgettes", ingredientsLasagne, "Plat", Arrays.asList("Plat chaud")));
-		this.listRecette.add(new Recette("Gateau au yaourt de soja et pommes", ingredientsGateauYaourtSojaPomme, "Dessert", Arrays.asList("Plat froid","Gateau")));
+		this.listRecette.add(new Recette("Gateau au yaourt de soja et pommes", ingredientsGateauYaourtSojaPomme, "Dessert", Arrays.asList("Plat chaud","Gateau")));
+		this.listRecette.add(new Recette("Pizza vegan aux legumes d hiver", ingredientsPizzaVeganLegumesDHiver, "Plat", Arrays.asList("Plat chaud","Pizza")));
+		this.listRecette.add(new Recette("Gaspacho", ingredientsGaspacho, "Plat", Arrays.asList("Plat froid")));
 	}
 
 	public static RecetteVeganSingleton getInstance() {
